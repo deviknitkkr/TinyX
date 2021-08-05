f04717892892dae2adc8b553c3f3cd1da12c9d26
Vikas <41135360+deviknitkkr@users.noreply.github.com>
2021-08-05 22:12:52 +0530

    Update TinyXCustomListener.java

--- src/main/java/TinyXCustomListener.java
+++ src/main/java/TinyXCustomListener.java
@@ -9,11 +9,10 @@ public class TinyXCustomListener extends TinyXBaseListener {
 
 	@Override
 	public void exitShow(TinyXParser.ShowContext ctx) {
-		if (ctx.INT() != null) {
+		if (ctx.INT() != null)
 			System.out.println(ctx.INT().getText());
-		} else if (ctx.VAR() != null) {
+		else if (ctx.VAR() != null)
 			System.out.println(this.variableMap.get(ctx.VAR().getText()));
-		}
 	}
 
 	@Override
@@ -23,13 +22,9 @@ public class TinyXCustomListener extends TinyXBaseListener {
 	 	String value=(ctx.VAR().size()>1)?ctx.VAR(1).getText():ctx.INT().getText();
 	 	
 	 	if(ctx.VAR().size()>1)
-	 	{
 	 		this.variableMap.put(variableName,variableMap.get(value));
-	 	}
 	 	else
-	 	{
 		 	this.variableMap.put(variableName, Integer.parseInt(value));
-	 	}
 	}
 
 }
